65f4b7975f8fc87f0421d53f323323508c5673c8
Cephou <laurent.noyon@gmail.com>
2017-10-06 15:09:02 +0200

    init

---
+++ inc/header.php
@@ -0,0 +1,26 @@
+<html>
+  <head>
+  	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  	<title>Formation XML</title>
+    <link rel="stylesheet" href="css/fonts.css" />
+    <link rel="stylesheet" href="css/materialize.css" />
+    <link rel="stylesheet" href="css/theme.css">
+    <script src="js/jquery.min.js"></script>
+    <script src="js/materialize.min.js"></script>
+  </head>
+  <body>
+    <header>
+      <div id="header" class="navbar-fixed" style="z-index: 998;">
+        <nav>
+          <div class="container">
+            <div class="nav-wrapper">
+              <a href="index.php" class="brand-logo center">Gestion de contacts</a>
+              <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+              <ul class="left hide-on-med-and-down">
+                <li><a href="index.php"><i class="material-icons left">home</i>INDEX</a></li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </header>
---
+++ index.php
@@ -0,0 +1,58 @@
+<?php include('inc/header.php'); ?>
+
+<div class='container-fluid blue-grey lighten-4' style='padding: 20px;'>
+<div class="row">
+
+<?php
+
+$dom = new DomDocument();
+$dom->load("repertoire.xml");
+
+// if (!$dom->validate()) {
+//   echo "Not validated";
+//   return false;
+// } else {
+//   echo "Validated";
+// }
+
+$contactList = $dom->getElementsByTagName("contact");
+foreach($contactList as $contact) {
+
+?>
+<div class="col s12 m4">
+  <div class="card">
+   <div class="card-content white-text <?= $contact->getAttribute("color"); ?> darken-1">
+     <span class="card-title">
+       <?= $contact->getElementsByTagName("firstname")->item(0)->nodeValue; ?>
+       <b><?= $contact->getElementsByTagName("name")->item(0)->nodeValue; ?></b>
+     </span>
+   </div>
+   <div class="card-action">
+     <?php
+     $emailList = $contact->getElementsByTagName("email");
+     foreach ($emailList as $email) { ?>
+       <?= $email->nodeValue ?><br/>
+     <?php } ?>
+   </div>
+  </div>
+</div>
+
+<?php
+
+}
+
+// $nouveauPays = $dom->createElement("pays");
+// $nomPays = $dom->createTextNode("Royaume-Uni");
+//
+// $nouveauPays->appendChild($nomPays);
+// $europe = $dom->getElementsByTagName("europe")->item(0);
+// $europe->appendChild($nouveauPays);
+//
+// $dom->save("file.xml");
+
+?>
+
+</div>
+</div>
+
+<?php include('inc/footer.php'); ?>
